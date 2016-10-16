85c932251068d1a940e59fafd232bd4400436150
Matthew Healy <matthew.healy@fanduel.com>
2016-10-16 18:07:45 +0100

    Add doc comment to TableSection type

--- TableControlling/TableControlling/Model/TableSection.swift
+++ TableControlling/TableControlling/Model/TableSection.swift
@@ -1,10 +1,24 @@
 import Foundation
 
+/**
+ A `TableSection` is a view model which represents a section in a `UITableView`.
+ 
+ It has optional and optional `Header` and `Footer`, and a non-optional array 
+ of `Cell`s. The `Header`, `Footer` and `Cell` types are `Equatable` in order
+ for us to equate entire `TableSection`s.
+*/
 struct TableSection<Header: Equatable, Cell: Equatable, Footer: Equatable>: Equatable {
     let header: Header?
     let cells: [Cell]
     let footer: Footer?
     
+    /**
+     Creates a `TableSection` with the given parameters.
+     
+     - parameter header: The view model for the section's header. The default argument is `nil`.
+     - parameter cells: An array of view models for the sections' cells. The default argument is an empty array.
+     - parameter footer: The view model for the section's footer. The default argument is `nil`.
+    */
     init(header: Header? = nil, cells: [Cell] = [], footer: Footer? = nil) {
         self.header = header
         self.cells = cells
