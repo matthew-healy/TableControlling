81cdf0f3e513e53ac018a06faad4031850494af4
Matthew Healy <matthew.healy@fanduel.com>
2016-10-16 18:35:37 +0100

    Add Table doc comments

--- TableControlling/TableControlling/Model/Table.swift
+++ TableControlling/TableControlling/Model/Table.swift
@@ -1,3 +1,12 @@
+/**
+ A view model which represents the contents of a `UITableView`.
+ 
+ It has an optional `Header` and `Footer`, and a non-optional 
+ array of `Section`s (where a `Section` is a `typealias` for
+ `TableSection<SectionHeader, Cell, SectionFooter>`. The `Header`, 
+ `Footer` and `Section` types are `Equatable` in order for us to equate 
+ entire `Table`s.
+*/
 struct Table<
     Header: Equatable,
     SectionHeader: Equatable, Cell: Equatable, SectionFooter:Equatable,
@@ -9,6 +18,13 @@ struct Table<
     let sections: [Section]
     let footer: Footer?
     
+    /**
+     Creates a `Table` with given parameters.
+     
+     - parameter header: The view model for the table's header. The default argument is `nil`.
+     - parameter sections: An array of view models for the table's sections. The default argument is an empty array.
+     - parameter footer: The view model for the table's footer. The default argument is `nil`.
+    */
     init(header: Header? = nil, sections: [Section] = [], footer: Footer? = nil) {
         self.header = header
         self.sections = sections
