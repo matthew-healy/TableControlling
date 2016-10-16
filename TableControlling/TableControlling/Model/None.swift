a17fb68cfba18557866c21ddb0b435c911f3c925
Matthew Healy <matthew.healy@fanduel.com>
2016-10-16 17:19:29 +0100

    Add None type

---
+++ TableControlling/TableControlling/Model/None.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+/**
+ `None` represents the absence of a value.
+ 
+ It can be thought of as being similar to Swift's own `Void`
+ type, with the vital difference being that `None` is `Equatable`.
+ This allows us to use `None` in place of a generic type which must
+ be `Equatable`.
+ 
+ In theory this won't need to exist in a future verison of Swift, once
+ conditional protocol conformance is added.
+*/
+enum None: Equatable {}
+
+func ==(lhs: None, rhs: None) -> Bool {
+    return true
+}
