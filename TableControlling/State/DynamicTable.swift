823a76dc040a9724db9e2b7e661f76a7c1adb9aa
Matthew Healy <matthew@liamhealy.xyz>
2016-10-27 15:15:13 +0100

    Add DynamicTable method doc comments

--- TableControlling/State/DynamicTable.swift
+++ TableControlling/State/DynamicTable.swift
@@ -2,6 +2,10 @@
  A state model which represents the possible states for a dynamic `Table`.
  It can either be `ready` to display a `Table`, `loading` one, `displaying` one, 
  or showing an `Error`.
+ 
+ It has proxies for the `Table`s data methods - e.g. `numberOfSections`
+ which it passes on to the underlying `Table`, if it exists, or returns
+ sensible default values otherwise.
 */
 enum DynamicTable<
     Header: Equatable,
@@ -21,16 +25,37 @@ enum DynamicTable<
     /// The table has `failed` to load. This is the appropriate time to retry, or show an error.
     case failed(Error)
     
+    /**
+     The number of sections in the underlying `Table`, or `0` if one is not displayed.
+    */
     var numberOfSections: Int {
         guard case .displaying(let table) = self else { return 0 }
         return table.numberOfSections
     }
     
+    
+    /**
+     The number of `Cell`s in the given section of the underlying `Table`.
+     
+     - parameter section: The `Int` index of the requested section.
+     
+     - returns: The `numberOfItems` for that section of the `Table`,
+                or `0` if one is not displayed.
+    */
     func numberOfItems(inSection section: Int) -> Int {
         guard case .displaying(let table) = self else { return 0 }
         return table.numberOfItems(inSection: section)
     }
     
+    
+    /**
+     Fetches the item at the given index path of the underlying `Table`.
+     
+     - parameter indexPath: The `IndexPath` of the requested `Cell`.
+     
+     - returns: The result of calling `item(at:)` on the underlying `Table`,
+                or `nil` if one is not currently displayed.
+    */
     func item(at indexPath: IndexPath) -> Cell? {
         guard case .displaying(let table) = self else { return nil }
         return table.item(at: indexPath)
