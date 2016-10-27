71a280bf1882eba30bbcdfdcdb3271bf05e32d34
Matthew Healy <matthew@liamhealy.xyz>
2016-10-27 19:32:55 +0100

    Update AssertSymmetricallyEqual documentation

--- TableControllingTests/TestHelpers/AssertSymmetricallyEqual.swift
+++ TableControllingTests/TestHelpers/AssertSymmetricallyEqual.swift
@@ -4,6 +4,10 @@ import XCTest
  Asserts that the equality operation on an `Equatable` type `T` is symmetric.
  i.e., it asserts that `t_1 == t_2` if and only if `t_2 == t_1`.
  
+ This is a dumb function to need, because in reality equality should be an
+ equivalence relation, and therefore guarantee symmetry, but `XCTAssertEqual`
+ doesn't check in both directions, and having this cuts a whole lot of asserts.
+ 
  - parameter expression1: An `@autoclosure` which evaluates to something of type `T`.
  - parameter expression2: An `@autoclosure` which evaluates to something of type `T`.
  - parameter message: The message to print in case of failure. Defaults to "AssertSymmetricallyEqual failed."
