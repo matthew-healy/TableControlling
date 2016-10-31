f2dad76a21f33afde1809f37a73b442f8f07e616
Matthew Healy <matthew@liamhealy.xyz>
2016-10-31 13:10:04 +0000

    Add TableModelling protocol to describe various table models

---
+++ TableControlling/DataPropagating/TableModelling.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+/**
+ A type that models a `UITableView`.
+ 
+ Types which conform to `TableModelling` can respond to requests for
+ the `Table`'s number of sections, number of items in a particular 
+ section, or the item at a given `IndexPath`. `Table`, `StaticTable`
+ and `DynamicTable` all conform to `TableModelling`.
+*/
+protocol TableModelling {
+    associatedtype Cell
+    
+    /**
+     The number of sections to show in the table.
+    */
+    var numberOfSections: Int { get }
+    
+    /**
+     The number of `Cell`s in the given section.
+     
+     - parameter section: The `Int` value of the requested section.
+     
+     - returns: The number of cells in that section.
+    */
+    func numberOfItems(inSection section: Int) -> Int
+    
+    /** 
+     Fetches the `Cell` at the given `IndexPath` of the table.
+     
+     - parameter indexPath: The `IndexPath` of the requested `Cell`.
+     
+     - returns: The `Cell`, if it exists, or `nil` otherwise.
+    */
+    func item(at indexPath: IndexPath) -> Cell?
+}
