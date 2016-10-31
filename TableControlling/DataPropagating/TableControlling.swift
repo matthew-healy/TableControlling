faf2d45eb8c205a35918bced5f8a8cedd0f95a61
Matthew Healy <matthew@liamhealy.xyz>
2016-10-31 13:20:57 +0000

    Add TableControlling protocol

---
+++ TableControlling/DataPropagating/TableControlling.swift
@@ -0,0 +1,30 @@
+/**
+ A type which acts as a controller for an instance of `UITableView`, 
+ or some subclass.
+ 
+ Types which conform to `TableControlling` are only required to have
+ a `model` of the table, and a reference to the `view` itself. Other
+ behaviours which involve these properties are codified in protocols
+ which 'inherit' from `TableControlling`. Generally your types should
+ conform to those protocols instead of using `TableControlling` directly.
+*/
+protocol TableControlling {
+    associatedtype Model: TableModelling
+    associatedtype View
+    
+    associatedtype Header: Equatable
+    associatedtype SectionHeader: Equatable
+    associatedtype Cell: Equatable
+    associatedtype SectionFooter: Equatable
+    associatedtype Footer: Equatable
+    
+    /**
+     The view model underlying the table.
+    */
+    var model: Model { get set }
+    
+    /**
+     The table view which is being controlled.
+    */
+    var view: View { get }
+}
