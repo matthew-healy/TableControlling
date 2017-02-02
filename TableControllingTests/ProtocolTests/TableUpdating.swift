13b97f6ccc99d99a4b10b944c4a1cd44e5cd9601
Matthew Healy <matthew.healy@codeclan.com>
2017-02-02 17:47:37 +0000

    Add basic TableUpdating update method for static tables

--- TableControllingTests/ProtocolTests/TableUpdating.swift
+++ TableControllingTests/ProtocolTests/TableUpdating.swift
@@ -1,9 +1,28 @@
-//
-//  TableUpdating.swift
-//  TableControlling
-//
-//  Created by Matthew on 02/02/2017.
-//  Copyright © 2017 Matthew Healy. All rights reserved.
-//
+protocol TableUpdating {
+    associatedtype Model
+    associatedtype View
 
-import Foundation
+    associatedtype Header: Equatable
+    associatedtype SectionHeader: Equatable
+    associatedtype Cell: Equatable
+    associatedtype SectionFooter: Equatable
+    associatedtype Footer: Equatable
+
+    var model: Model { get }
+    var view: View { get }
+
+    func update()
+}
+
+extension TableUpdating where
+    Model == StaticTable<Header, SectionHeader, Cell, SectionFooter, Footer>,
+    View: DataReloadable
+{
+    typealias ContextualTable = StaticTable<Header, SectionHeader, Cell, SectionFooter, Footer>
+    func update() {
+        guard case .displaying = model else {
+            return
+        }
+        view.reloadData()
+    }
+}
--- TableControllingTests/ProtocolTests/TableUpdatingTests.swift
+++ TableControllingTests/ProtocolTests/TableUpdatingTests.swift
@@ -1,11 +1,43 @@
 import XCTest
+@testable import TableControlling
 
-class TableControlingExtensionTests: XCTestCase {
+class TableUpdatingTests: XCTestCase {
 
+    func test_update_staticTable_displaying_callsReloadData() {
+        let mockView = MockDataReloadable()
+        let displayingTable = BlankStaticTable.displaying(.create())
+        let sut = PartialMockTableUpdating(model: displayingTable, view: mockView)
+        sut.update()
+        XCTAssertTrue(mockView.didReloadData)
+    }
+
+    func test_update_staticTable_ready_doesNotCallReloadData() {
+        let mockView = MockDataReloadable()
+        let readyTable = BlankStaticTable.ready
+        let sut = PartialMockTableUpdating(model: readyTable, view: mockView)
+        sut.update()
+        XCTAssertFalse(mockView.didReloadData)
+    }
 }
 
-private class PartialMockStaticTableControlilng: TableControlling {
+private typealias BlankStaticTable = StaticTable<None, None, None, None, None>
+
+private class PartialMockTableUpdating: TableUpdating {
+    typealias Model = BlankStaticTable
+    typealias View = MockDataReloadable
+    typealias Header = None
+    typealias SectionHeader = None
+    typealias Cell = None
+    typealias SectionFooter = None
+    typealias Footer = None
     
+    let model: BlankStaticTable
+    let view: MockDataReloadable
+
+    init(model: BlankStaticTable, view: MockDataReloadable) {
+        self.model = model
+        self.view = view
+    }
 }
 
 private class MockDataReloadable: DataReloadable {
